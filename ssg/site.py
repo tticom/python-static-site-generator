20a5b35541f49fa6f14b92616e867989c99b3d1a
Niall Finlay <niall.finlay@gmail.com>
2022-05-12 14:35:53 +0100

    stuff

--- ssg/site.py
+++ ssg/site.py
@@ -7,7 +7,7 @@ class Site:
     self.dest = Path(dest)
 
   def create_dir(self, path):
-    directory = "{}/{}".format(self.dest, Path.relative_to(self.source))
+    directory = self.dest / path.relative_to(path)
     Path.mkdir(directory, parents=True, exist_ok=True)
 
   def build(self):
