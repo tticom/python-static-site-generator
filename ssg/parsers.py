79a831d051f1eb15e1630e8f6059569c12378215
Niall Finlay <niall.finlay@gmail.com>
2022-05-12 15:44:51 +0100

    words

---
+++ ssg/parsers.py
@@ -0,0 +1,21 @@
+from typing import List
+from pathlib import Path
+
+class Parser():
+  def __init__(self):
+    self.extensions: List[str] = []
+
+  def valid_extension(self, extension):
+    return extension in self.extensions
+  
+  def parse(self, path: Path, source: Path, dest: Path):
+    raise NotImplementedError
+
+  def read(self, path: Path):
+    with open(path) as file:
+      return file.read()
+
+  def write(self, path, dest, content, ext="html"):
+    full_path = dest / path.with_suffix(ext).name
+    with open(full_path, 'w') as file:
+      file.write(content)
