597fc475d981012abaa1ae99d02ebba518612597
Niall Finlay <niall.finlay@gmail.com>
2022-05-13 16:16:48 +0100

    content

--- ssg/content.py
+++ ssg/content.py
@@ -5,31 +5,40 @@ from PyYAML import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-  __delimiter = "^(?:-|\+){3}\s*$"
+  __delimiter = r"^(?:-|\+){3}\s*$"
   __regex = re.compile(__delimiter, re.MULTILINE)
 
   @classmethod
   def load(cls, string):
-    _, fm, content = Content.__regex.split(string, 2)
-    load(fm, Loader=FullLoader)
+    _, fm, content = cls.__regex.split(string, 2)
+    metadata = load(fm, Loader=FullLoader)
     return cls(metadata, content)
 
-  class Content:
-    def __init__(self, metadata, content):
-      self.data = metadata
-      self.data.append({"content": content})
+  def __init__(self, metadata, content):
+    self.data = metadata
+    self.data["content"] = content
 
-    @property
-    def body(self): return self.data["content"]
+  @property
+  def body(self): return self.data["content"]
 
-    @property
-    def type(self): return self.data["type"] if "type" in self.data else None
-    @property.setter
-    def type(self):
-      self.data["type"] = property(type)
+  @property
+  def type(self): return self.data["type"] if "type" in self.data else None
+  @property.setter
+  def type(self):
+    self.data["type"] = property(type)
 
-    def __getitem__(self, key: str):
-      return self.data[key] if key in self.data else None
+  def __getitem__(self, key: str):
+    return self.data[key]
 
-    def __iter__(self):
-      self.data.__iter__()
+  def __iter__(self):
+    self.data.__iter__()
+
+  def __len__(self):
+    return len(self.data)
+    
+  def __repr__(self):
+    data = {}
+    for key, value in self.data.items():
+      if key != "content":
+        data[key] = value
+    return str(data)
