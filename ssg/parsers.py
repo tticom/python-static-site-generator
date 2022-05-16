bef55136e382870a2e7110c8a6c200a3cca3c234
Niall Finlay <niall.finlay@gmail.com>
2022-05-16 11:20:17 +0100

    parsers

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,11 @@
 from typing import List
 from pathlib import Path
 import shutil
+import sys
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
+
 
 class Parser():
   extensions: List[str] = []
@@ -24,7 +29,25 @@ class Parser():
     shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
-  extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+  extensions: List[str] = [".jpg", ".png", ".gif", ".css", ".html"]
 
   def parse(self, path: Path, source: Path, dest: Path):
     self.copy(path, source, dest)
+
+class MarkdownParser(Parser):
+  extensions: List[str] = [".md", ".markdown"]
+
+  def parse(self, path: Path, source: Path, dest: Path):
+    content = Content.load(self.read(path=path))
+    html = markdown(content.body)
+    self.write(path, dest, html)
+    sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+class ReStructuredTextParser(Parser):
+  extensions: List[str] = [".rst"]
+
+  def parse(self, path: Path, source: Path, dest: Path):
+    content = Content.load(self.read(path=path))
+    html = publish_parts(content.body, writer_name="html5")
+    self.write(path, dest, html["html_body"])
+    sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 from msilib.schema import Directory
 from pathlib import Path
+import sys
 
 class Site:
   def __init__(self, source, dest, parsers=None):
@@ -29,4 +30,8 @@ class Site:
     if parser is not None:
       parser.parse(path, self.source, self.dest)
     else:
-      print("Not Implemented")
+      self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+
+  @staticmethod()
+  def error(message):
+    sys.stderr.write("\x1b[1;31m{}\n".format(message))
